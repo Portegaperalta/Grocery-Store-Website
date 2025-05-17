ec8b6d0e5f9ca05c3f972a434287c82e04aa790d
pablothecoder <ortegaperaltap@gmail.com>
2025-05-17 13:14:50 -0400

    html css and js files updated

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-
-//dropdown menu 
+//dropdown menu variables
 const dropDownMenuButton = document.querySelector('#dropDownMenuBtN')
 const dropDownMenu = document.querySelector('.dropdown-menu-left')
 
+//main content variables
+const mainContentAdd = document.querySelector('#mainContentAdd')
+const addMoreBtn = document.querySelector('#addMoreBtn')
+const quitaddBtn = document.querySelector('#AddQuitBtn')
+
+const addToCartButtons = document.querySelectorAll('button')
+
+//dropdown menu events
 dropDownMenuButton.addEventListener('click', () => {
   dropDownMenu.classList.toggle('dropdown-menu-left-show')
+  mainContent.classList.toggle('main-content-dropdown')
+})
+
+// main content events
+
+quitaddBtn.addEventListener('click', () => {
+  mainContentAdd.style.display = 'none'
 })
